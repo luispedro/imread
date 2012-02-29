3f8e7b7a04164a0ecacd992a2eafa936b2257ac2
Luis Pedro Coelho <luis@luispedro.org>
2012-02-29 16:34:37 +0000

    RFCT Simplify code with macros
    Sometimes macros are the right answer in C++.
    
    Add std::string constructors too. There is no reason to just use const
    char*.

--- imread/lib/errors.h
+++ imread/lib/errors.h
@@ -3,56 +3,31 @@
 
 #ifndef LPC_ERRORS_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
 #define LPC_ERRORS_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
+#include <string>
 #include <exception>
 
-struct NotImplementedError : std::exception {
-    NotImplementedError()
-        :w(0)
-        { }
-    NotImplementedError(const char* w)
-        :w(w)
-        { }
+#define DECLARE_IMREAD_ERROR_TYPE(TypeName, DefaultMsg) \
+    struct TypeName : std::exception { \
+        TypeName(const char* e) \
+            :w(e) \
+            { } \
+        TypeName(std::string e) \
+            :w(e) \
+            { } \
+        TypeName() \
+            :w(DefaultMsg) \
+            { } \
+        ~TypeName() throw() { } \
+        \
+        \
+        const char* what() const throw() { return w.c_str(); } \
+        \
+        std::string w;\
+    }
 
-    const char* what() const throw() { return w ? w : "Not Implemented"; }
-
-    const char* w;
-};
-
-struct ProgrammingError : std::exception {
-    ProgrammingError()
-        :w(0)
-        { }
-    ProgrammingError(const char* w)
-        :w(w)
-        { }
-
-    const char* what() const throw() { return w ? w : "Programming Error"; }
-
-    const char* w;
-};
-
-struct CannotReadError : std::exception {
-    CannotReadError(const char* w)
-        :w(w)
-        { }
-    CannotReadError()
-        :w(0)
-        { }
-
-    const char* what() const throw() { return w ? w : "Read Error"; }
-    const char* w;
-
-};
-struct CannotWriteError : std::exception {
-    CannotWriteError(const char* w)
-        :w(w)
-        { }
-    CannotWriteError()
-        :w(0)
-        { }
-
-    const char* what() const throw() { return w ? w : "Write Error"; }
-    const char* w;
-};
+DECLARE_IMREAD_ERROR_TYPE(CannotReadError, "Read Error");
+DECLARE_IMREAD_ERROR_TYPE(CannotWriteError, "Write Error");
+DECLARE_IMREAD_ERROR_TYPE(NotImplementedError, "Not Implemented");
+DECLARE_IMREAD_ERROR_TYPE(ProgrammingError, "Programming Error");
 
 #endif // LPC_ERRORS_H_INCLUDE_GUARD_WED_FEB__1_16_34_50_WET_2012
