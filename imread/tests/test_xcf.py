24cc1efc71f8380700fd446c7ef9d8e25e0e3f0f
cgohlke <cgohlke@uci.edu>
2012-08-01 00:36:21 -0700

    Fix indentation

--- imread/tests/test_xcf.py
+++ imread/tests/test_xcf.py
@@ -3,7 +3,7 @@ import sys
 from imread import imread
 
 if not sys.platform.startswith('win'):
-   # there is no native xcf2png utility for Windows
+    # there is no native xcf2png utility for Windows
     def test_xcf():
         im = imread('imread/tests/data/diag.xcf')
         assert im.shape == (8, 8, 3)
