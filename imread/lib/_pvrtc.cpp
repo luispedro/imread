ce2f3695cb176feb2c50d66cef03ea455f557daa
Alexander Böhn <fish2000@gmail.com>
2012-11-13 12:53:42 -0500

    It compiles! Relatedly, I am not what you might call a C++ "wizard".

--- imread/lib/_pvrtc.cpp
+++ imread/lib/_pvrtc.cpp
@@ -7,18 +7,20 @@
 
 #include "pvr.h"
 
-std::auto_ptr<Image> WebPFormat::read(byte_source* src, ImageFactory* factory) {
+std::auto_ptr<Image> PVRTCFormat::read(byte_source* src, ImageFactory* factory) {
     std::vector<byte> data = full_data(*src);
     PVRTexture pvr;
 
-    int res = pvr.load(str(data.begin(), data.end()));
+    int res = pvr.loadApplePVRTC(&data[0], data.size());
     if (res != PVR_LOAD_OKAY && res != PVR_LOAD_UNKNOWN_TYPE) {
         throw CannotReadError("imread.imread._pvrtc: File isn't a valid PVRTC texture.");
     }
 
     std::auto_ptr<Image> output(factory->create(8, pvr.height, pvr.width, 4));
     if (pvr.data) {
-        output->rowp_as<uint8_t>(0) = pvr.data
+        /* clearly this is not the best approach */
+        uint8_t* rowp = output->rowp_as<uint8_t>(0);
+        memcpy(&pvr.data[0], &rowp, pvr.width*pvr.height*4);
     } else {
         throw CannotReadError("imread.imread._pvrtc: Error reading PVRTC file.");
     }
