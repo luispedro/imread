cc72f821457326b98cb38d1640a963608c355d3a
Luis Pedro Coelho <luis@luispedro.org>
2013-01-09 16:21:43 +0000

    TST Add ``verbose`` argument to tests.run()

---
+++ imread/tests/__init__.py
@@ -0,0 +1,9 @@
+def run(verbose=False):
+    import nose
+    from os import path
+    currentdir = path.dirname(__file__)
+    updir = path.join(currentdir, '..')
+    argv = ['', '--exe', '-w', updir]
+    if verbose:
+        argv.append('--verbose')
+    nose.run('imread', argv=argv)
