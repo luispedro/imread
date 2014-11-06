e3294cc94f2666632a95289d7f61cbc0ba1ee008
Luis Pedro Coelho <luis@luispedro.org>
2014-11-06 11:58:47 +0100

    TST Auto skip xcf when xcf2png is missing

--- imread/tests/test_xcf.py
+++ imread/tests/test_xcf.py
@@ -1,10 +1,24 @@
 import sys 
 from imread import imread
 from . import file_path
+from nose import SkipTest
 
-if not sys.platform.startswith('win'):
+def has_xcf2png():
     # there is no native xcf2png utility for Windows
-    def test_xcf():
-        im = imread(file_path('diag.xcf'))
-        assert im.shape == (8, 8, 3)
-        assert im.max(2).diagonal().sum() == 0
+    if sys.platform.startswith('win'):
+        return False
+    try:
+        from shutil import which
+    except:
+        from distutils.spawn import find_executable as which
+
+    c = which('xcf2png')
+    return (c is not None)
+
+
+def test_xcf():
+    if not has_xcf2png():
+        raise SkipTest
+    im = imread(file_path('diag.xcf'))
+    assert im.shape == (8, 8, 3)
+    assert im.max(2).diagonal().sum() == 0
