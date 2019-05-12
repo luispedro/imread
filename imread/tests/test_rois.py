6132167abdd135ae902b3b0d1d7d18cbe48854b6
Luis Pedro Coelho <luis@luispedro.org>
2019-05-12 16:57:30 +0200

    TST Make test position independent

--- imread/tests/test_rois.py
+++ imread/tests/test_rois.py
@@ -5,7 +5,7 @@ from . import file_path
 
 
 def test_rois_smoke():
-    rois = ijrois.read_roi_zip('./imread/tests/data/rois.zip')
+    rois = ijrois.read_roi_zip(file_path('rois.zip'))
     assert len(rois) == 4
-    r = ijrois.read_roi(open('./imread/tests/data/0186-0099.roi', 'rb'))
+    r = ijrois.read_roi(open(file_path('0186-0099.roi'), 'rb'))
     assert any([np.array_equal(ri, r) for ri in rois])
