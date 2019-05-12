6942c0b598b00d2d723f05bc56ddfb3edd4417b8
Luis Pedro Coelho <luis@luispedro.org>
2019-05-12 16:57:52 +0200

    BLD Add wheel building infrastructure

---
+++ build-wheels.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Based on https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
+
+set -ev
+
+PLAT=manylinux2010_x86_64
+
+yum -y install libjpeg-devel libpng-devel libtiff-devel libwebp-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install imread --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" imread)
+done
+
