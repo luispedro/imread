93bff15a14cc2a52cede8167deea45c0fa9dd120
Luis Pedro Coelho <luis@luispedro.org>
2022-05-24 17:09:39 +0200

    BLD Do not attempt to build pypy wheels
    They currently fail due to missing libraries
    
    /cc #40

--- build-wheels.sh
+++ build-wheels.sh
@@ -9,13 +9,21 @@ PLAT=manylinux2014_x86_64
 yum -y install libjpeg-devel libpng-devel libtiff-devel libwebp-devel
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYVER in \
+       cp36-cp36m \
+       cp37-cp37m \
+       cp38-cp38 \
+       cp39-cp39 \
+       cp310-cp310 \
+       ; do
+
+    PYBIN="/opt/python/${PYVER}/bin"
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/imread*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
 done
 
